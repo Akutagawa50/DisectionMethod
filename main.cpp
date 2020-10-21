2c802b1850b024991f66cfa763f1170b30d5ed0b
Akutagawa50 <ryusuke0525a@gmail.com>
2020-10-21 12:38:50 +0900

    収束判定を改良

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@
 
 using namespace std;
 
-double x_cosx(double x){// f(x)=x+cos(x) の関数
-    return x + cos(x);
+double func(double x){// f(x)=x+cos(x) の関数
+    return x + 10000*cos(x);
 }
 
 int main(){
     double rng_max = 10000.0;   //最初に探す範囲の最大値（値は適当）
     double rng_min = -rng_max;  //最初に探す範囲の最小値（値は適当）
     double h = rng_max/10.0;    //範囲を探すときの分割数
-    double error = 0.000000001; //収束判定のときの誤差
+    double error = 0.00000001; //収束判定のときの誤差
     double xmin=0, xmax=0;      //それぞれxの最小値，最大値を保存する変数
     double x;                   //xminとxmaxの平均を保存する
 
     for(int i=0; i<50; i++){    //50回動かす
         for(double n=rng_min; n<rng_max; n+=h){//分割した範囲を1つずつ見ていく
-            double y1 = x_cosx(n);      //それぞれのyの値を計算
-            double y2 = x_cosx(n+h);    
+            double y1 = func(n);      //それぞれのyの値を計算
+            double y2 = func(n+h);    
             if(y1<0.0 && y2>0.0){       //y1とy2の間に0があったらbreak
                 xmin = n;
                 xmax = n+h;
@@ -32,7 +32,7 @@ int main(){
             }
         }
         x = (xmin+xmax)/2;                      //xminとxmaxの平均を計算
-        if(rng_max-x<error && x-rng_min<error){ //収束判定
+        if(func(x)>-error && func(x)<error){ //収束判定
             cout << i+1 << "回目で収束しました" << endl;
             break;
         }
@@ -43,7 +43,7 @@ int main(){
     }
     //結果を出力
     cout <<"x= "<< fixed << setprecision(6) << x << endl;
-    cout << "f(x) =" << fixed << setprecision(6) << x_cosx(x) << endl;
+    cout << "f(x) =" << fixed << setprecision(6) << func(x) << endl;
 
     return 0;
 }
