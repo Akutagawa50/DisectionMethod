f72d5dba20db2f0e6f5f1f7268fe77b148c444a0
Akutagawa50 <ryusuke0525a@gmail.com>
2020-10-21 12:55:27 +0900

    一連の流れを関数化

--- main.cpp
+++ main.cpp
@@ -8,14 +8,13 @@ double func(double x){// f(x)=x+cos(x) の関数
     return x + cos(x);
 }
 
-int main(){
+bool FindSolution(double *x){
     double rng_max = 10000.0;   //最初に探す範囲の最大値（値は適当）
     double rng_min = -rng_max;  //最初に探す範囲の最小値（値は適当）
     double h = rng_max/10.0;    //範囲を探すときの分割数
     double error = 0.00000001; //収束判定のときの誤差
     double xmin=0, xmax=0;      //それぞれxの最小値，最大値を保存する変数
-    double x=0;                   //xminとxmaxの平均を保存する
-
+    
     for(int i=0; i<50; i++){    //50回動かす
         for(double n=rng_min; n<rng_max; n+=h){//分割した範囲を1つずつ見ていく
             double y1 = func(n);      //それぞれのyの値を計算
@@ -31,19 +30,31 @@ int main(){
                 break;
             }
         }
-        x = (xmin+xmax)/2;                      //xminとxmaxの平均を計算
-        if(func(x)>-error && func(x)<error){ //収束判定
-            cout << i+1 << "回目で収束しました" << endl;
-            break;
+        *x = (xmin+xmax)/2;                      //xminとxmaxの平均を計算
+        if(func(*x)>-error && func(*x)<error){ //収束判定
+            //cout << i+1 << "回目で収束しました" << endl;
+            return true;
         }
         
         rng_min = xmin; //探索範囲の最小値を更新
         rng_max = xmax; //探索範囲の最大値を更新
         h /= 10.0;      //分割する幅を更新
     }
-    //結果を出力
-    cout <<"x= "<< fixed << setprecision(6) << x << endl;
-    cout << "f(x) =" << fixed << setprecision(6) << func(x) << endl;
+    return false;
+}
+
+int main(){
+    double x=0;
+
+    if(FindSolution(&x)){//収束したら
+        //結果を出力
+        cout <<"x= "<< fixed << setprecision(6) << x << endl;
+        cout << "f(x)= " << fixed << setprecision(6) << fabs(func(x)) << endl;
+    }
+    else{
+        cout << "収束しませんでした" <<endl;
+    }
+    
 
     return 0;
 }
